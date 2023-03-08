9a029ecc11fb7e89d74874ab1ea2bbe9447d9c7a
Anish Sarkar <anish1696@gmail.com>
2023-03-08 23:36:19 +0530

    final

--- swap.c
+++ swap.c
@@ -1,1 +1,16 @@
 //swap 2 variables without using 3rd variable
+#include <stdio.h>
+
+
+int main()
+{
+    int a,b;
+    printf("Enter 2 numbers: \n");
+    scanf("%d %d", &a, &b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("The swapped numbers are %d %d", a, b);
+    return 0;
+    
+}
