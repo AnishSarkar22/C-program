edc2c5cb951f941fa10b7944bddd7b3a656b013e
Anish Sarkar <anish1696@gmail.com>
2023-03-08 23:57:49 +0530

    s

--- Basic-Calc.c
+++ Basic-Calc.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+int main()
 {
-    int a,b;
+    int a, b;
     printf("Enter two numbers: ");
     scanf("%d%d", &a, &b);
 
-    printf("Addition is %d\n", a+b);
-    printf("Substraction is %d\n", a-b);
-    printf("Multiplication is %d\n", a*b);
-    printf("Division is %f\n", (float)a/b);
+    printf("Addition is %d\n", a + b);
+    printf("Substraction is %d\n", a - b);
+    printf("Multiplication is %d\n", a * b);
+    printf("Division is %f\n", (float)a / b);
     return 0;
 }
--- swap.c
+++ swap.c
@@ -1,16 +1,14 @@
-//swap 2 variables without using 3rd variable
+// swap 2 variables without using 3rd variable
 #include <stdio.h>
-
 
 int main()
 {
-    int a,b;
+    int a, b;
     printf("Enter 2 numbers: \n");
     scanf("%d %d", &a, &b);
-    a=a+b;
-    b=a-b;
-    a=a-b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
     printf("The swapped numbers are %d %d", a, b);
     return 0;
-    
 }
