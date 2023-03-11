b51e07f2bf1983a6c7306cd3910974a5daf1013e
Anish Sarkar <anish1696@gmail.com>
2023-03-11 22:21:37 +0530

    s2

--- structs.c
+++ structs.c
@@ -19,9 +19,21 @@ int main ()
     strcpy(student1.name, "Anuj");
     strcpy(student1.major, "IT");
 
+    struct Student student2;
+    student2.age=21;
+    student2.gpa=3.4;
+    strcpy(student2.name, "Akash");
+    strcpy(student2.major, "Business");    
+    
+    
     printf("%f\n", student1.gpa);
     printf("%s\n", student1.major);
-    printf("%s\n", student1.name);
+    printf("%s\n\n", student1.name);
+
+    printf("%f\n", student2.gpa);
+    printf("%s\n", student2.major);
+    printf("%s\n", student2.name);
+
 
     return 0;
 }
