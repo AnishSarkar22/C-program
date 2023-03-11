6fc606661dfec3a3ea6d07bd63ea118e787a349d
Anish Sarkar <anish1696@gmail.com>
2023-03-12 00:39:46 +0530

    game

--- guess_game.c
+++ guess_game.c
@@ -1,17 +1,27 @@
-//create a guessing game
-#include<stdio.h>
-#include<stdlib.h>
+// create a guessing game and having limits
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int secretNumber = 5;
-    int guess;
-    
-    while (guess != secretNumber)
+    int guess, guess_count = 0, guess_limit = 3, outOfGuess = 0;
+
+    while (guess != secretNumber && outOfGuess == 0)
+    {
+        if (guess_count < guess_limit)
         {
             printf("Enter a number: ");
             scanf("%d", &guess);
+            guess_count++;
         }
-    printf("You Win!");
+        else
+            outOfGuess = 1;
+    }
+    if (outOfGuess == 1)
+        printf("Out of guesses");
+    else
+        printf("You Win!");
 
+    return 0;
 }
