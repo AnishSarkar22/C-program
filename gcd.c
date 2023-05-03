0764214f83614fa628f495d223a1a669f7e38aa6
Anish Sarkar <anish16916@gmail.com>
2023-05-03 08:01:08 +0530

    gcd

---
+++ gcd.c
@@ -0,0 +1,22 @@
+//find G.C.D using recursion
+#include <stdio.h>
+
+int hcf(int a, int b);
+
+int main()
+{
+    int a, b;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &a, &b);
+    printf("G.C.D of %d and %d is %d.", a, b, hcf(a, b));
+    return 0;
+}
+
+//using Euclidean algorithm
+int hcf(int a, int b)
+{
+    if (b != 0)
+        return hcf(b, a % b);
+    else
+        return a;
+}
