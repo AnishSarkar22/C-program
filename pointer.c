23c1c26e6af6a4697d00a7a7b17bbb219281017b
Anish Sarkar <anish16916@gmail.com>
2023-05-03 08:14:53 +0530

    p

--- pointer.c
+++ pointer.c
@@ -1,28 +1,26 @@
-//Pointer is a type of data that is a memory address
-// "&n" can be literally translated as the address of n
+// Pointer is a type of data that is a memory address
+//  "&n" can be literally translated as the address of n
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    int age =30;
-    int *pAge = &age;    //creating a pointer for age
-    double gpa =3.4;
-    double *pGpa = &gpa;    //creating a pointer for gpa
-    char grade ='A';
-    char *pGrade = &grade;    //creating a pointer for grade 
+    int age = 30;
+    int *pAge = &age; // creating a pointer for age
+    double gpa = 3.4;
+    double *pGpa = &gpa; // creating a pointer for gpa
+    char grade = 'A';
+    char *pGrade = &grade; // creating a pointer for grade
 
-    printf("age: %p\ngpa: %p\ngrade: %p\n\n", &age, &gpa, &grade);       //output of memory addresses
-    printf("age: %p\ngpa: %p\ngrade: %p\n\n", pAge, pGpa, pGrade);       //output of memory addresses with pointers    
+    printf("age: %p\ngpa: %p\ngrade: %p\n\n", &age, &gpa, &grade); // output of memory addresses
+    printf("age: %p\ngpa: %p\ngrade: %p\n\n", pAge, pGpa, pGrade); // output of memory addresses with pointers
 
-    printf("age: %d\n", *pAge);   //dereferencing the 'Age' pointer 
-    printf("age: %d", *&age);   //also dereferencing the 'Age' pointer 
+    printf("age: %d\n", *pAge); // dereferencing the 'Age' pointer
+    printf("age: %d", *&age);   // also dereferencing the 'Age' pointer
 
-
-    return 0;   
+    return 0;
 }
-
 
 /* Deferencing a pointer means to grab the actual value stored at the memory address of that pointer.
     We use "*" to dereference a pointer.  */
