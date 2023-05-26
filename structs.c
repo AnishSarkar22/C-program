d85593e061e2a8cab4fada8eebaeebf02560ba52
Anish Sarkar <anish16916@gmail.com>
2023-05-27 00:30:41 +0530

    struct

--- structs.c
+++ structs.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct Student
 {
@@ -8,23 +8,21 @@ struct Student
     char major[50];
     int age;
     double gpa;
-
 };
 
-int main ()
+int main()
 {
     struct Student s1, s2;
-    s1.age=22;
-    s1.gpa=3.2;
+    s1.age = 22;
+    s1.gpa = 3.2;
     strcpy(s1.name, "Anuj");
     strcpy(s1.major, "IT");
 
-    s2.age=21;
-    s2.gpa=3.4;
+    s2.age = 21;
+    s2.gpa = 3.4;
     strcpy(s2.name, "Akash");
-    strcpy(s2.major, "Business");    
-    
-    
+    strcpy(s2.major, "Business");
+
     printf("%f\n", s1.gpa);
     printf("%s\n", s1.major);
     printf("%s\n\n", s1.name);
