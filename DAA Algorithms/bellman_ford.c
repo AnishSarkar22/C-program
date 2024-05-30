e5c032fa28445bb5230ac467b5ffb96eb73f3614
Anish Sarkar <anish16916@gmail.com>
2024-05-30 10:51:58 +0530

    algo

--- DAA Algorithms/bellman_ford.c
+++ DAA Algorithms/bellman_ford.c
@@ -1,66 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
 #define E 6
 #define V 4
 
-typedef struct e{
+typedef struct e {
     int u;
     int v;
     int weight;
 } Edge;
 
-int main()
-{
+void bellman(Edge edge[], int src);
+void printSol(int dist[]);
 
-    struct e edge[E];
+int main() {
+    Edge edge[E];
     int i;
-    for(i=0;i<E;i++){
-        printf("Enter the edge in the format(u,v,wieght) %d:\n",i+1);
-        scanf("%d", &edge[i].u);
-        scanf("%d", &edge[i].v);
-        scanf("%d", &edge[i].weight);
+    for (i = 0; i < E; i++) {
+        printf("Enter the edge in the format (u, v, weight) %d:\n", i + 1);
+        scanf("%d %d %d", &edge[i].u, &edge[i].v, &edge[i].weight);
     }
 
-
-
-    bellman(edge,0);
+    bellman(edge, 0);
 
     return 0;
 }
 
-void bellman(struct e edge[], int src){
-
-    int i,j;
-
+void bellman(Edge edge[], int src) {
+    int i, j;
     int dist[V];
 
-    for(i=0;i<V;i++){
+    for (i = 0; i < V; i++) {
         dist[i] = INT_MAX;
     }
 
     dist[src] = 0;
 
-    for(i=0;i<V;i++){
-        for(j=0;j<E;j++){
-            int u = edge[i].u;
-            int v = edge[i].v;
-            int weight = edge[i].weight;
-            if(dist[u]+weight < dist[v]){
-                dist[v] = dist[u]+weight;
+    for (i = 1; i <= V - 1; i++) {
+        for (j = 0; j < E; j++) {
+            int u = edge[j].u;
+            int v = edge[j].v;
+            int weight = edge[j].weight;
+            if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
             }
         }
     }
 
-    printSol(dist);
-
-}
-
-void printSol(int dist[]){
-
-    int i;
-    for(i=0;i<V;i++){
-        printf("%d\t\t\t%d\n", i, dist[i]);
+    // Check for negative-weight cycles
+    for (j = 0; j < E; j++) {
+        int u = edge[j].u;
+        int v = edge[j].v;
+        int weight = edge[j].weight;
+        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+            printf("Graph contains negative weight cycle\n");
+            return;
+        }
     }
 
+    printSol(dist);
+}
+
+void printSol(int dist[]) {
+    int i;
+    for (i = 0; i < V; i++) {
+        printf("%d\t\t\t%d\n", i, dist[i]);
+    }
 }
